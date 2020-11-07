67d68b9ac5a336525a381381efb6da88173b2fbe
denismwangi <denniske992@gmail.com>
2020-11-07 15:02:06 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
+const API_URL = 'https://api.icndb.com/jokes/random';
+
+
+
 function App() {
+  const [joke, setjoke] = useState('');
+
+  const generateJoke = () => {
+    fetch(API_URL)
+      .then(res => res.json())
+      .then(data => setjoke(data.value.joke));
+
+  }
+  useEffect(() => {
+    generateJoke();
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="box">
+      <h1>chuck norris jokes generator</h1>
+      <p dangerouslySetInnerHTML={{ __html: joke }} />
+
+      <button onClick={generateJoke}>get new jokes<span role="img" aria-label="laugh emoji"> 😀</span></button>
+
     </div>
   );
 }
